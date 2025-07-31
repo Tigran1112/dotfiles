f7db74cb4feeed41a94b809fb7fce9ac02717274
branch <main>
2025-07-31 23:26:04 +0300

    0.16

--- configuration.nix
+++ configuration.nix
@@ -107,7 +107,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim wget git kitty zsh 
-    gtk  glib libGL xorg.libX11 xorg.libXrandr xorg.libXi mesa alsaLib vulcan-loader nss nspr libdrm ffmpeg icu zlib libpulseaudio
+    gtk glib libGL xorg.libX11 xorg.libXrandr xorg.libXi mesa alsaLib vulcan-loader nss nspr libdrm ffmpeg zlib libpulseaudio
     hyprland waybar wofi hyprpaper hyprcursor
     nautilus
     unityhub vscode blender
--- install.sh
+++ install.sh
@@ -4,11 +4,7 @@ sudo cp -rf waybar ~/.config/waybar
 sudo cp -rf wofi ~/.config/wofi
 sudo cp -rf configuration.nix /etc/nixos/configuration.nix
 sudo cp -rf wall1.jpg ~/Pictures/wall1.jpg
+mkdir ~/.local/share/applications
+sudo cp -rf unityhub.desktop ~/.local/share/applications/unityhub.desktop
 sudo nixos-rebuild switch
-echo "Do you want reboot PC?"
-echo -n 1 -p answer
-if [[ $answer =~ ^[Yy]$  || -z "$answer" ]]; then
-	reboot
-else
-	echo "Done"
-fi
+reboot
